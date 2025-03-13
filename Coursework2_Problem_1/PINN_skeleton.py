a7775088564c5180bb6f2970c8c5869beeb99acc
Derek cblgpu <jd976@cam.ac.uk>
2025-03-13 13:27:34 +0000

    use cuda, jet color map

--- Coursework2_Problem_1/PINN_skeleton.py
+++ Coursework2_Problem_1/PINN_skeleton.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import numpy as np
 import scipy.io
 import matplotlib.tri as mtri
+from tqdm import tqdm
 
 # Initialize wandb project (replace "Plate_PINN" and "your_username" with your project name and wandb username)
 wandb.init(project="Plate_PINN")
@@ -13,11 +14,15 @@ wandb.init(project="Plate_PINN")
 # Log configuration parameters to wandb
 config = wandb.config
 config.learning_rate = 1e-3
-config.iterations = int(5e4)
+config.iterations = int(100)
 config.Disp_layer = [2, 300, 300, 2]
 config.Stress_layer = [2, 400, 400, 3]
 config.E = 10.0
 config.mu = 0.3
+
+# ------------------------- Added for CUDA -------------------------
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# --------------------------------------------------------------------
 
 # Define the DenseNet neural network
 class DenseNet(nn.Module):
@@ -25,21 +30,17 @@ class DenseNet(nn.Module):
         super(DenseNet, self).__init__()
 
         self.n_layers = len(layers) - 1
-
         assert self.n_layers >= 1
-
         self.layers = nn.ModuleList()
 
         for j in range(self.n_layers):
             self.layers.append(nn.Linear(layers[j], layers[j + 1]))
-
             if j != self.n_layers - 1:
                 self.layers.append(nonlinearity())
 
     def forward(self, x):
         for _, l in enumerate(self.layers):
             x = l(x)
-
         return x
 
 ############################# Data Processing #############################
@@ -47,28 +48,30 @@ class DenseNet(nn.Module):
 path = './Coursework2_Problem_1/plate_data.mat'
 data = scipy.io.loadmat(path)
 torch.set_default_tensor_type(torch.DoubleTensor)
-L_boundary = torch.tensor(data['L_boundary'], dtype=torch.float64)
-R_boundary = torch.tensor(data['R_boundary'], dtype=torch.float64)
-T_boundary = torch.tensor(data['T_boundary'], dtype=torch.float64)
-B_boundary = torch.tensor(data['B_boundary'], dtype=torch.float64)
-C_boundary = torch.tensor(data['C_boundary'], dtype=torch.float64)
-Boundary   = torch.tensor(data['Boundary'], dtype=torch.float64, requires_grad=True)
+
+# Load and move tensors to CUDA
+L_boundary = torch.tensor(data['L_boundary'], dtype=torch.float64).to(device)
+R_boundary = torch.tensor(data['R_boundary'], dtype=torch.float64).to(device)
+T_boundary = torch.tensor(data['T_boundary'], dtype=torch.float64).to(device)
+B_boundary = torch.tensor(data['B_boundary'], dtype=torch.float64).to(device)
+C_boundary = torch.tensor(data['C_boundary'], dtype=torch.float64).to(device)
+Boundary   = torch.tensor(data['Boundary'], dtype=torch.float64, requires_grad=True).to(device)
 
 # truth solution from FEM
-disp_truth = torch.tensor(data['disp_data'], dtype=torch.float64)
+disp_truth = torch.tensor(data['disp_data'], dtype=torch.float64).to(device)
 
 # connectivity matrix - this helps you to plot the figure but we do not need it for PINN
-t_connect  = torch.tensor(data['t'].astype(float), dtype=torch.float64)
+t_connect  = torch.tensor(data['t'].astype(float), dtype=torch.float64).to(device)
 
 # all collocation points
-x_full = torch.tensor(data['p_full'], dtype=torch.float64,requires_grad=True)
+x_full = torch.tensor(data['p_full'], dtype=torch.float64, requires_grad=True).to(device)
 
 # collocation points excluding the boundary
-x = torch.tensor(data['p'], dtype=torch.float64, requires_grad=True)
+x = torch.tensor(data['p'], dtype=torch.float64, requires_grad=True).to(device)
 
 # This chooses 50 fixed points from the truth solution, which we will use for part (e)
-rand_index = torch.randint(0, len(x_full), (50,))
-disp_fix = disp_truth[rand_index,:]
+rand_index = torch.randint(0, len(x_full), (50,)).to(device)
+disp_fix = disp_truth[rand_index, :]
 
 # We will use two neural networks for the problem:
 # NN1: to map the coordinates [x,y] to displacement u
@@ -82,14 +85,14 @@ disp_fix = disp_truth[rand_index,:]
 Disp_layer = config.Disp_layer  # Displacement network layers
 Stress_layer = config.Stress_layer  # Stress network layers
 
-stress_net = DenseNet(Stress_layer,nn.Tanh) # Note we choose hyperbolic tangent as an activation function here
-disp_net =  DenseNet(Disp_layer,nn.Tanh)
+stress_net = DenseNet(Stress_layer, nn.Tanh).to(device) # Note we choose hyperbolic tangent as an activation function here
+disp_net = DenseNet(Disp_layer, nn.Tanh).to(device)
 
-# Define material properties
+# Define material properties and move to CUDA
 E = config.E        
 mu = config.mu        
 stiff = E/(1-mu**2)*torch.tensor([[1,mu,0],[mu,1,0],[0,0,(1-mu)/2]]) # Hooke's law for plane stress
-stiff = stiff.unsqueeze(0)
+stiff = stiff.unsqueeze(0).to(device)
 
 # PINN requires super large number of iterations to converge (on the order of 50e^3-100e^3)
 iterations = config.iterations
@@ -111,171 +114,181 @@ optimizer = torch.optim.Adam(params, lr=config.learning_rate)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.5)
 
 # Training loop
-for epoch in range(iterations):
-    scheduler.step()
-    optimizer.zero_grad()
+with tqdm(total=iterations, initial=0, desc="Training Epochs", unit="epoch") as pbar_epoch:
+    for epoch in range(iterations):
+        optimizer.zero_grad()
 
-    # To compute stress from stress net
-    sigma = stress_net(x)
-    # To compute displacement from disp net
-    disp = disp_net(x)
+        # To compute stress from stress net
+        sigma = stress_net(x)
+        # To compute displacement from disp net
+        disp = disp_net(x)
 
-    # displacement in x direction
-    u = disp[:,0]
-    # displacement in y direction
-    v = disp[:,1]
+        # displacement in x direction
+        u = disp[:,0]
+        # displacement in y direction
+        v = disp[:,1]
 
-    # find the derivatives
-    dudx = torch.autograd.grad(u, x, grad_outputs=torch.ones_like(u),create_graph=True)[0]
-    dvdx = torch.autograd.grad(v, x, grad_outputs=torch.ones_like(v),create_graph=True)[0]
+        # find the derivatives
+        dudx = torch.autograd.grad(u, x, grad_outputs=torch.ones_like(u),create_graph=True)[0]
+        dvdx = torch.autograd.grad(v, x, grad_outputs=torch.ones_like(v),create_graph=True)[0]
 
-    # Define strain components
-    e_11 = dudx[:, 0].unsqueeze(1)
-    e_22 = dvdx[:, 1].unsqueeze(1)
-    e_12 = 0.5 * (dudx[:, 1] + dvdx[:, 0]).unsqueeze(1)
-    e = torch.cat((e_11, e_22, e_12), 1).unsqueeze(2)
+        # Define strain components
+        e_11 = dudx[:, 0].unsqueeze(1)
+        e_22 = dvdx[:, 1].unsqueeze(1)
+        e_12 = 0.5 * (dudx[:, 1] + dvdx[:, 0]).unsqueeze(1)
+        e = torch.cat((e_11, e_22, e_12), 1).unsqueeze(2)
 
-    # Define augment stress
-    sig_aug = torch.bmm(stiff, e).squeeze(2)
+        # Define augment stress
+        sig_aug = torch.bmm(stiff, e).squeeze(2)
 
-    # Define constitutive loss - forcing the augment stress to be equal to the neural network stress
-    loss_cons = loss_func(sig_aug, sigma)
+        # Define constitutive loss - forcing the augment stress to be equal to the neural network stress
+        loss_cons = loss_func(sig_aug, sigma)
 
-    # find displacement and stress at the boundaries
-    disp_bc = disp_net(Boundary)
-    sigma_bc = stress_net(Boundary)
-    u_bc = disp_bc[:,0]
-    v_bc = disp_bc[:,1]
+        # find displacement and stress at the boundaries
+        disp_bc = disp_net(Boundary)
+        sigma_bc = stress_net(Boundary)
+        u_bc = disp_bc[:,0]
+        v_bc = disp_bc[:,1]
 
-    # Compute the strain and stresses at the boundary
-    dudx_bc = torch.autograd.grad(u_bc, Boundary, grad_outputs=torch.ones_like(u_bc), create_graph=True)[0]
-    dvdx_bc = torch.autograd.grad(v_bc, Boundary, grad_outputs=torch.ones_like(v_bc), create_graph=True)[0]
+        # Compute the strain and stresses at the boundary
+        dudx_bc = torch.autograd.grad(u_bc, Boundary, grad_outputs=torch.ones_like(u_bc), create_graph=True)[0]
+        dvdx_bc = torch.autograd.grad(v_bc, Boundary, grad_outputs=torch.ones_like(v_bc), create_graph=True)[0]
 
-    e_11_bc = dudx_bc[:,0].unsqueeze(1)
-    e_22_bc = dvdx_bc[:,1].unsqueeze(1)
-    e_12_bc = 0.5 * (dudx_bc[:, 1] + dvdx_bc[:, 0]).unsqueeze(1)
-    e_bc = torch.cat((e_11_bc,e_22_bc,e_12_bc), 1).unsqueeze(2)
+        e_11_bc = dudx_bc[:,0].unsqueeze(1)
+        e_22_bc = dvdx_bc[:,1].unsqueeze(1)
+        e_12_bc = 0.5 * (dudx_bc[:, 1] + dvdx_bc[:, 0]).unsqueeze(1)
+        e_bc = torch.cat((e_11_bc,e_22_bc,e_12_bc), 1).unsqueeze(2)
 
-    sig_aug_bc = torch.bmm(stiff_bc, e_bc).squeeze(2)
+        sig_aug_bc = torch.bmm(stiff_bc, e_bc).squeeze(2)
 
-    # force the augment stress to agree with the NN stress at the boundary
-    loss_cons_bc = loss_func(sig_aug_bc, sigma_bc)
+        # force the augment stress to agree with the NN stress at the boundary
+        loss_cons_bc = loss_func(sig_aug_bc, sigma_bc)
 
-    #============= Equilibrium (PDE residual) ===================#
-    sig_11 = sigma[:, 0]
-    sig_22 = sigma[:, 1]
-    sig_12 = sigma[:, 2]
+        #============= Equilibrium (PDE residual) ===================#
+        sig_11 = sigma[:, 0]
+        sig_22 = sigma[:, 1]
+        sig_12 = sigma[:, 2]
 
-    dsig11dx = torch.autograd.grad(sig_11, x, grad_outputs=torch.ones_like(sig_11), create_graph=True)[0]
-    dsig22dx = torch.autograd.grad(sig_22, x, grad_outputs=torch.ones_like(sig_22), create_graph=True)[0]
-    dsig12dx = torch.autograd.grad(sig_12, x, grad_outputs=torch.ones_like(sig_12), create_graph=True)[0]
+        dsig11dx = torch.autograd.grad(sig_11, x, grad_outputs=torch.ones_like(sig_11), create_graph=True)[0]
+        dsig22dx = torch.autograd.grad(sig_22, x, grad_outputs=torch.ones_like(sig_22), create_graph=True)[0]
+        dsig12dx = torch.autograd.grad(sig_12, x, grad_outputs=torch.ones_like(sig_12), create_graph=True)[0]
 
-    eq_x1 = dsig11dx[:, 0] + dsig12dx[:, 1]
-    eq_x2 = dsig12dx[:, 0] + dsig22dx[:, 1]
+        eq_x1 = dsig11dx[:, 0] + dsig12dx[:, 1]
+        eq_x2 = dsig12dx[:, 0] + dsig22dx[:, 1]
 
-    # zero body forces
-    f_x1 = torch.zeros_like(eq_x1)
-    f_x2 = torch.zeros_like(eq_x2)
+        # zero body forces
+        f_x1 = torch.zeros_like(eq_x1)
+        f_x2 = torch.zeros_like(eq_x2)
 
-    loss_eq1 = loss_func(eq_x1, f_x1)
-    loss_eq2 = loss_func(eq_x2, f_x2)
+        loss_eq1 = loss_func(eq_x1, f_x1)
+        loss_eq2 = loss_func(eq_x2, f_x2)
 
-    #========= Boundary Conditions ========================#
-    # Prescribed tractions
-    tau_R = 0.1  # Right boundary normal traction
-    tau_T = 0.0  # Top boundary traction
-    u_L = disp_net(L_boundary)
-    u_B = disp_net(B_boundary)
-    sig_R = stress_net(R_boundary)
-    sig_T = stress_net(T_boundary)
-    sig_C = stress_net(C_boundary)
+        #========= Boundary Conditions ========================#
+        # Prescribed tractions
+        tau_R = 0.1  # Right boundary normal traction
+        tau_T = 0.0  # Top boundary traction
+        u_L = disp_net(L_boundary)
+        u_B = disp_net(B_boundary)
+        sig_R = stress_net(R_boundary)
+        sig_T = stress_net(T_boundary)
+        sig_C = stress_net(C_boundary)
 
-    # Symmetry boundary condition left
-    loss_BC_L = loss_func(u_L[:,0], torch.zeros_like(u_L[:,0]))
-    # Symmetry boundary condition bottom
-    loss_BC_B = loss_func(u_B[:, 1], torch.zeros_like(u_B[:, 1]))
-    # Force boundary condition right
-    loss_BC_R = loss_func(sig_R[:, 0], tau_R*torch.ones_like(sig_R[:, 0])) \
-                + loss_func(sig_R[:, 2],  torch.zeros_like(sig_R[:, 2]))
+        # Symmetry boundary condition left
+        loss_BC_L = loss_func(u_L[:,0], torch.zeros_like(u_L[:,0]))
+        # Symmetry boundary condition bottom
+        loss_BC_B = loss_func(u_B[:, 1], torch.zeros_like(u_B[:, 1]))
+        # Force boundary condition right
+        loss_BC_R = loss_func(sig_R[:, 0], tau_R*torch.ones_like(sig_R[:, 0])) \
+                    + loss_func(sig_R[:, 2],  torch.zeros_like(sig_R[:, 2]))
 
-    loss_BC_T = + loss_func(sig_T[:, 1], tau_T*torch.ones_like(sig_T[:, 1]))   \
-                + loss_func(sig_T[:, 2],  torch.zeros_like(sig_T[:, 2]))
+        loss_BC_T = + loss_func(sig_T[:, 1], tau_T*torch.ones_like(sig_T[:, 1]))   \
+                    + loss_func(sig_T[:, 2],  torch.zeros_like(sig_T[:, 2]))
 
-    # traction free on circle
-    loss_BC_C = loss_func(sig_C[:,0]*C_boundary[:,0]+sig_C[:,2]*C_boundary[:,1], torch.zeros_like(sig_C[:, 0]))  \
-                + loss_func(sig_C[:,2]*C_boundary[:,0]+sig_C[:,1]*C_boundary[:,1], torch.zeros_like(sig_C[:, 0]))
+        # traction free on circle
+        loss_BC_C = loss_func(sig_C[:,0]*C_boundary[:,0]+sig_C[:,2]*C_boundary[:,1], torch.zeros_like(sig_C[:, 0]))  \
+                    + loss_func(sig_C[:,2]*C_boundary[:,0]+sig_C[:,1]*C_boundary[:,1], torch.zeros_like(sig_C[:, 0]))
 
-    # Define the total loss function:
-    # 1. loss_eq1 + loss_eq2: PDE residual error (equilibrium)
-    # 2. loss_cons + loss_cons_bc: Constitutive loss (enforcing Hooke's law consistency)
-    # 3. loss_BC_L + loss_BC_B + loss_BC_R + loss_BC_T + loss_BC_C: Boundary condition losses
-    loss = loss_eq1 + loss_eq2 + loss_cons + loss_cons_bc + \
-           loss_BC_L + loss_BC_B + loss_BC_R + loss_BC_T + loss_BC_C
+        # Define the total loss function:
+        # 1. loss_eq1 + loss_eq2: PDE residual error (equilibrium)
+        # 2. loss_cons + loss_cons_bc: Constitutive loss (enforcing Hooke's law consistency)
+        # 3. loss_BC_L + loss_BC_B + loss_BC_R + loss_BC_T + loss_BC_C: Boundary condition losses
+        loss = loss_eq1 + loss_eq2 + loss_cons + loss_cons_bc + \
+            loss_BC_L + loss_BC_B + loss_BC_R + loss_BC_T + loss_BC_C
 
-    # ======= Uncomment below for part (e) if additional data loss is used =======================
-    # data_loss_fix
-    #x_fix = x_full[rand_index, :]
-    #u_fix = disp_net(x_fix)
-    #loss_fix = loss_func(u_fix,disp_fix)
-    #loss = loss_eq1+loss_eq2+loss_cons+loss_BC_L+loss_BC_B+loss_BC_R+loss_BC_T+loss_BC_C+loss_cons_bc + 100*loss_fix
+        # ======= Uncomment below for part (e) if additional data loss is used =======================
+        # data_loss_fix
+        #x_fix = x_full[rand_index, :]
+        #u_fix = disp_net(x_fix)
+        #loss_fix = loss_func(u_fix,disp_fix)
+        #loss = loss_eq1+loss_eq2+loss_cons+loss_BC_L+loss_BC_B+loss_BC_R+loss_BC_T+loss_BC_C+loss_cons_bc + 100*loss_fix
 
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
 
-    loss.backward()
-    print('loss', loss.item(), 'iter', epoch)
-    optimizer.step()
-
-    # Log training metrics to wandb
-    wandb.log({
-        "epoch": epoch,
-        "PDE_residual_loss": loss_eq1.item() + loss_eq2.item(),          # loss_eq1 + loss_eq2: PDE residual error (equilibrium)
-        "constitutive_loss": loss_cons.item() + loss_cons_bc.item(),        # loss_cons + loss_cons_bc: Constitutive loss (Hooke's law consistency)
-        "boundary_loss": (loss_BC_L.item() + loss_BC_B.item() + 
-                        loss_BC_R.item() + loss_BC_T.item() + loss_BC_C.item()),  # Boundary condition losses
-        "total_loss": loss.item()
-    })
-
+        # Log training metrics to wandb
+        wandb.log({
+            "epoch": epoch,
+            "PDE_residual_loss": loss_eq1.item() + loss_eq2.item(),          # loss_eq1 + loss_eq2: PDE residual error (equilibrium)
+            "constitutive_loss": loss_cons.item() + loss_cons_bc.item(),        # loss_cons + loss_cons_bc: Constitutive loss (Hooke's law consistency)
+            "boundary_loss": (loss_BC_L.item() + loss_BC_B.item() + 
+                            loss_BC_R.item() + loss_BC_T.item() + loss_BC_C.item()),  # Boundary condition losses
+            "total_loss": loss.item()
+        })
+    
+        # Update progress bar
+        if (epoch + 1) % 10 == 0:
+            pbar_epoch.update(10)
+            pbar_epoch.set_postfix({"Total loss": f"{loss.item():.4e}"})
+            
 # Plot the stress field
 u_full = disp_net(x_full)
 stress_full = stress_net(x_full)
 
-xx = x_full[:,0].detach().numpy()
-yy = x_full[:,1].detach().numpy()
-sig11 = stress_full[:,1].detach().numpy()
+xx = x_full[:,0].detach().cpu().numpy()
+yy = x_full[:,1].detach().cpu().numpy()
+sig11 = stress_full[:,1].detach().cpu().numpy()
 
-connect = (t_connect - 1).detach().numpy()
+connect = (t_connect - 1).detach().cpu().numpy()
 
 triang = mtri.Triangulation(xx, yy, connect)
 
-u_11 = u_full[:,0].detach().numpy()
+u_11 = u_full[:,0].detach().cpu().numpy()
 
 u = u_full[:, 0]
 v = u_full[:, 1]
 
+# Compute gradients and stress field
 dudx = torch.autograd.grad(u, x_full, grad_outputs=torch.ones_like(u), create_graph=True)[0]
 dvdx = torch.autograd.grad(v, x_full, grad_outputs=torch.ones_like(v), create_graph=True)[0]
 
 e_11 = dudx[:, 0].unsqueeze(1)
 e_22 = dvdx[:, 1].unsqueeze(1)
 e_12 = 0.5 * (dudx[:, 1] + dvdx[:, 0]).unsqueeze(1)
-
-e = torch.cat((e_11, e_22, e_12), 1)
-e = e.unsqueeze(2)
-
+e = torch.cat((e_11, e_22, e_12), 1).unsqueeze(2)
 sigma = torch.bmm(stiff_full, e).squeeze(2)
 
-plt.figure(2)
-plt.clf()
-plt.tricontourf(triang, sigma[:, 0].detach().numpy())
-plt.colorbar()
+# Create and save the contour plot with colormap
+plt.figure(figsize=(8, 6))
+# Specify the colormap (here 'jet' is used to mimic MATLAB's jet colormap)
+contour = plt.tricontourf(triang, sigma[:, 0].detach().cpu().numpy(), cmap='jet')
+cb = plt.colorbar(contour)
+plt.title("Contour Plot of Sigma_11")
+plt.xlabel("X")
+plt.ylabel("Y")
 
-# Save the plot to the "results" directory instead of showing it
 os.makedirs('results', exist_ok=True)
-plt.savefig(os.path.join('results', 'contour_plot.png'))
+plot_path = os.path.join('results', 'contour_plot.png')
+plt.savefig(plot_path)
 plt.close()
 
+# Log the contour plot image to wandb
+wandb.log({"contour_plot": wandb.Image(plot_path)})
+
 # Optionally, save the model checkpoint and log it to wandb
+checkpoint_path = os.path.join('results', 'model_checkpoint.pth')
 torch.save({
     'stress_net': stress_net.state_dict(),
     'disp_net': disp_net.state_dict(),
-}, os.path.join('results', 'model_checkpoint.pth'))
-wandb.save(os.path.join('results', 'model_checkpoint.pth'))
+}, checkpoint_path)
+wandb.save(checkpoint_path)
